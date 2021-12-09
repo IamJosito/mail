045b9a4cf8d8406df0b5eca9e04b9d5aecd3d509
IamJosito <joserr0209@gmail.com>
2021-12-09 10:35:16 +0100

    Add mail viewer

--- index.py
+++ index.py
@@ -1,5 +1,4 @@
-import json
-
+import email
 import imapclient.exceptions
 from imapclient import IMAPClient
 from flask import Flask, render_template, jsonify, request
@@ -8,6 +7,8 @@ from flask import Flask, render_template, jsonify, request
 app = Flask(__name__)
 
 mails = {}
+
+host = 'imap.gmail.com'
 
 @app.route('/')
 def index():
@@ -26,11 +27,11 @@ def fetch_mails():
     else:
         return jsonify(status=404)
 
+
 @app.route('/login', methods=['POST'])
 def login():
     try:
         if mails.get(str(request.form['mail'])) is None:
-            host = 'imap.gmail.com'
             server = IMAPClient(host=host, use_uid=True)
             mails[str(request.form['mail'])] = str(request.form['password'])
             server.login(str(request.form['mail']), str(request.form['password']))
@@ -49,5 +50,36 @@ def login():
 def delete_mail():
     del mails[request.form['mail']]
     return jsonify(status=204)
+
+
+@app.route("/get_inbox", methods = ['POST'])
+def get_inbox():
+    mail = request.form['mail']
+    password = mails.get(mail)
+    mails_inbox = {}
+    try:
+        server = IMAPClient(host=host, use_uid=True)
+        server.login(str(mail), str(password))
+        server.select_folder("INBOX")
+        messages = server.search('ALL')
+        index = 0
+        for uid, message_data in server.fetch(messages, 'RFC822').items():
+            email_message = email.message_from_bytes(message_data[b'RFC822'])
+            if email_message.is_multipart():
+                for part in email_message.get_payload():
+                    mails_inbox[index] = {
+                        "from": email_message.get("From"),
+                        "subject": email_message.get("Subject"),
+                        "payload": part.get_payload()
+                    }
+            index += 1
+        print(mails_inbox)
+        return jsonify(status=200, mails = mails_inbox)
+
+    except imapclient.exceptions.LoginError:
+        print("ERROR")
+        del mails[request.form['mail']]
+        return jsonify(status=500)
+
 
 app.run()
